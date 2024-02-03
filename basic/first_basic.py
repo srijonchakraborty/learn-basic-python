05254d3598d58ee3394d3ddfb2c74c1ea7f96a13
srijonchak <srijonchak@live.com>
2024-02-04 01:57:31 +0600

    First python code

---
+++ basic/first_basic.py
@@ -0,0 +1,16 @@
+print("Hi srijon");
+
+
+age:int = 30;
+name: str="Srijon Chakraborty";
+height:float=5.6;
+is_student=False;
+
+#print("Info of Name:{name}, age:{age}, height:{height}, Studentship Status:{is_student} ")
+# letter f is like Console.Writeline($"Name:{name}") like this
+print(f"Info of Name: {name}, age: {age}, height: {height}, Studentship Status: {is_student}");
+
+
+
+
+
